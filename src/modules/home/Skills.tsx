0adf1bbe34ae58550a3bb922dec8cfb9d8dd4bfe
JatsukinG <marketplacepremiumdev@gmail.com>
2024-12-06 20:18:54 -0500

    feat:updating skills

--- src/modules/home/Skills.tsx
+++ src/modules/home/Skills.tsx
@@ -1,16 +1,22 @@
-import { FaReact, FaNodeJs, FaPython, FaGitAlt } from 'react-icons/fa'
-import { SiTailwindcss, SiNextdotjs, SiMongodb, SiFirebase } from 'react-icons/si'
+import clsx from 'clsx'
+import { FaCss3Alt, FaGitAlt, FaHtml5, FaJs, FaPython, FaReact } from 'react-icons/fa'
+import { SiAstro, SiDjango, SiGraphql, SiNextdotjs, SiTailwindcss, SiTypescript } from 'react-icons/si'
 
 const skills = [
+  { name: 'Html', icon: FaHtml5 },
+  { name: 'Css', icon: FaCss3Alt },
+  { name: 'Javascript', icon: FaJs },
+  { name: 'Typescript', icon: SiTypescript },
   { name: 'React', icon: FaReact },
   { name: 'Next.js', icon: SiNextdotjs },
-  { name: 'Node.js', icon: FaNodeJs },
-  { name: 'Python', icon: FaPython },
-  { name: 'TailwindCSS', icon: SiTailwindcss },
-  { name: 'MongoDB', icon: SiMongodb },
-  { name: 'Firebase', icon: SiFirebase },
   { name: 'Git', icon: FaGitAlt },
+  { name: 'TailwindCSS', icon: SiTailwindcss },
+  { name: 'GraphQl', icon: SiGraphql },
+  { name: 'Python', icon: FaPython },
+  { name: 'Django', icon: SiDjango },
+  { name: 'Astro', icon: SiAstro },
 ]
+
 
 const Skills = () => {
   return (
@@ -19,19 +25,24 @@ const Skills = () => {
           <h2 className="mb-24 text-3xl md:text-4xl font-bold text-center text-neutral-800 dark:text-white">
             Skills
           </h2>
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8">
-            {skills.map((skill, index) => {
-              const Icon = skill.icon
-              return (
-                  <div
-                      key={index}
-                      className="flex flex-col items-center justify-center bg-white dark:bg-neutral-700 shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105"
-                  >
-                    <Icon className="text-4xl text-purple-500 mb-2"/>
-                    <p className="text-neutral-800 dark:text-neutral-200 font-semibold">{skill.name}</p>
-                  </div>
-              )
-            })}
+          <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-4 md:gap-8">
+            {
+              skills.map((skill, index) => {
+                const Icon = skill.icon
+                return (
+                    <div
+                        key={index}
+                        className={clsx([
+                          'flex flex-col items-center justify-center bg-white shadow-lg rounded-lg p-4',
+                          'dark:bg-neutral-700 hover:scale-105 duration-300',
+                        ])}
+                    >
+                      <Icon className="text-4xl text-purple-500 mb-2"/>
+                      <p className="text-neutral-800 dark:text-neutral-200 font-semibold">{skill.name}</p>
+                    </div>
+                )
+              })
+            }
           </div>
         </div>
       </section>
