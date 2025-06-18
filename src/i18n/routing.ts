91538e18e34d99855140e2ce1cf3507b1682e7e1
Julian Trujillo <julian@n1.app>
2025-06-18 16:15:37 -0500

    Added language selector.
    Refactored experience section

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,4 +3,4 @@ import { defineRouting, type RoutingConfig } from 'next-intl/routing'
 export const routing = defineRouting({
   locales: ['en', 'es'],
   defaultLocale: 'en',
-} as RoutingConfig<any, any, any, any>)
+} as RoutingConfig<never, never, never, never>)
